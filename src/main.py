f62f00e8b52a33035c6ca23fb0b9a16c3d330d6e
Victor LE CORRE <victor.le-corre@epita.fr>
2022-09-21 14:47:25 +0200

    [FEAT] xlsx_builder: Add functions to build sheets skeleton

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
 from xlsx_builder import *
 
 def main():
-    print("main() called")
-    build_schedule()
+    time_slots = [
+            '23h-00h',
+            '00h-01h',
+            '01h-02h',
+            '02h-03h'
+        ]
+
+    workers = [
+            'worker 1',
+            'worker 2',
+            'worker 3',
+            'worker 4'
+        ]
+
+    stands = [
+            'Bar',
+            'Entrée',
+            'Toilettes',
+            'Vestiaire'
+        ]
+
+    build_schedule(time_slots, workers, stands)
--- src/xlsx_builder.py
+++ src/xlsx_builder.py
@@ -1,8 +1,36 @@
 import xlsxwriter as xw
 
-def build_schedule():
+def build_schedule(time_slots, workers, stands):
     wb = xw.Workbook('schedule.xlsx')
     global_sheet = wb.add_worksheet('Global')
     individual_sheet = wb.add_worksheet('Individual')
 
+    build_global_sheet_skeleton(global_sheet, time_slots, stands)
+
+    build_individual_sheet_skeleton(individual_sheet, time_slots, workers)
+
     wb.close()
+
+def build_global_sheet_skeleton(sheet, time_slots, stands):
+    row = 1
+    col = 1
+
+    for time_slot in time_slots:
+        sheet.write(row, 0, time_slot)
+        row += 1
+
+    for stand in stands:
+        sheet.write(0, col, stand)
+        col += 1
+
+def build_individual_sheet_skeleton(sheet, time_slots, workers):
+    row = 1
+    col = 1
+
+    for time_slot in time_slots:
+        sheet.write(0, col, time_slot)
+        col += 1
+
+    for worker in workers:
+        sheet.write(row, 0, worker)
+        row += 1
