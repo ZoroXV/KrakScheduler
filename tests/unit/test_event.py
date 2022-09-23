8fa0571bca1bea79ead2cd5381c36bc6260013c8
Victor LE CORRE <victor.le-corre@epita.fr>
2022-09-23 23:36:56 +0200

    [TEST] Event: Add unit tests for all getter

--- tests/unit/test_event.py
+++ tests/unit/test_event.py
@@ -2,9 +2,35 @@ import pytest
 import datetime
 import krakscheduler as ks
 
-def test_get_name():
-    start_hour = datetime.datetime(2022, 9, 10, 23, 00, 00)
-    end_hour = datetime.datetime(2022, 9, 11, 6, 00, 00)
-    event = ks.Event("test_event", 0, 0, start_hour, end_hour)
+class TestEvent():
+    def setup_class(self):
+        self.start_hour = datetime.datetime(2022, 9, 10, 23, 00, 00)
+        self.end_hour = datetime.datetime(2022, 9, 11, 6, 00, 00)
+        self.event = ks.Event("test_event", 0, 0, self.start_hour, self.end_hour)
 
-    assert event.get_name() == "test_event"
+    def test_get_name(self):
+        assert self.event.get_name() == "test_event"
+
+    def test_get_nb_stands(self):
+        assert self.event.get_nb_stands() == 0
+
+    def test_get_nb_workers(self):
+        assert self.event.get_nb_workers() == 0
+
+    def test_get_start_hour(self):
+        assert self.event.get_start_hour() == self.start_hour
+
+    def test_get_end_hour(self):
+        assert self.event.get_end_hour() == self.end_hour
+
+    def test_get_seconds_duration(self):
+        assert self.event.get_seconds_duration() == 25200
+
+    def test_get_hours_duration(self):
+        assert self.event.get_hours_duration() == 7
+
+    def test_get_stands_list(self):
+        assert not self.event.get_stands_list()
+
+    def test_get_workers_list(self):
+        assert not self.event.get_workers_list()
