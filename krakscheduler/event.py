6937593a50c325c120994e32229adf2d92372897
Victor LE CORRE <victor.le-corre@epita.fr>
2022-09-25 17:14:56 +0200

    [REFACTOR] Event: Remove useless attributes nb_stands and nb_workers

--- krakscheduler/event.py
+++ krakscheduler/event.py
@@ -1,21 +1,19 @@
 class Event:
-    def __init__(self, name, nb_stands, nb_workers, start_hour, end_hour):
+    def __init__(self, name, start_hour, end_hour, stands_list=[], workers_list=[]):
         self.name = name
-        self.nb_stands = nb_stands
-        self.nb_workers = nb_workers
         self.start_hour = start_hour
         self.end_hour = end_hour
-        self.stands_list = []
-        self.workers_list = []
+        self.stands_list = stands_list
+        self.workers_list = workers_list
 
     def get_name(self):
         return self.name
 
     def get_nb_stands(self):
-        return self.nb_stands
+        return len(self.stands_list)
 
     def get_nb_workers(self):
-        return self.nb_workers
+        return len(self.workers_list)
 
     def get_start_hour(self):
         return self.start_hour
@@ -37,16 +35,12 @@ class Event:
 
     def add_stand(self, stand):
         self.stands_list.append(stand)
-        self.nb_stands +=1
 
     def remove_stand(self, stand):
         self.stands_list.remove(stand)
-        self.nb_stands -=1
 
     def add_worker(self, worker):
         self.workers_list.append(worker)
-        self.nb_workers +=1
 
     def remove_worker(self, worker):
         self.workers_list.remove(worker)
-        self.nb_workers -=1
