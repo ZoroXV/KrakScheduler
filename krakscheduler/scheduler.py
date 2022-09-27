c8c63247eda432d10d08151566604f5a02ebaa32
Victor LE CORRE <victor.le-corre@epita.fr>
2022-09-27 14:28:30 +0200

    [REFACTOR] Scheduler: Remove useless attributes stands_list and workers_list

--- krakscheduler/scheduler.py
+++ krakscheduler/scheduler.py
@@ -4,23 +4,7 @@ class Scheduler:
     schedule = {}
 
     def __init__(self, event):
-        # Event object
         self.event = event
-        # Stand object list
-        self.stands_list = event.get_stands_list()
-        # Worker object list
-        self.workers_list = event.get_workers_list()
-
-    def fill_schedule(self):
-        workers = self.get_workers_list()
-        for stand in self.get_stands_list():
-            # Shifts
-            for i in range(len(stand.get_staff_needed())):
-                while(stand.get_free_places(i) > 0):
-                    current_worker = random.choice(workers)
-                    while current_worker.is_working(i) != 0:
-                        current_worker = random.choice(workers)
-                    stand.add_worker(current_worker, i)
 
     def get_schedule(self):
         return self.schedule
@@ -28,8 +12,12 @@ class Scheduler:
     def get_event(self):
         return self.event
 
-    def get_stands_list(self):
-        return self.stands_list
-
-    def get_workers_list(self):
-        return self.workers_list
+    def fill_schedule(self):
+        workers = self.event.get_workers_list()
+        for stand in self.event.get_stands_list():
+            for i in range(len(stand.get_staff_needed())):
+                while(stand.get_free_places(i) > 0):
+                    current_worker = random.choice(workers)
+                    while current_worker.is_working(i) != 0:
+                        current_worker = random.choice(workers)
+                    stand.add_worker(current_worker, i)
