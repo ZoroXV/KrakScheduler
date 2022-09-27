d766b6d757a137abef02dd1a3562d246002e17db
Victor LE CORRE <victor.le-corre@epita.fr>
2022-09-27 14:16:10 +0200

    [FEAT] main.py: Update to create manager workers

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import datetime
 def main():
     event = ks.build_event()
 
-    list_workers = []
     for i in range(42):
-        worker = ks.Worker("worker_" + str(i), 1, [0,1,1,1,1,0,0], "staff")
-        list_workers.append(worker)
+        worker = ks.Worker("worker_" + str(i), [0,1,1,1,1,0,0])
+        event.add_worker(worker)
+
+    #Init Stand Manager
+    for stand in event.get_stands_list():
+        worker = ks.Worker(stand.get_name() + "_manager", [0,1,1,1,1,0,0], True)
         event.add_worker(worker)
 
     event.display()
