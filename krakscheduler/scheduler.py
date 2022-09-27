8b7a34c2b08907ef7afa6b782a823aefeb6fe39f
Victor LE CORRE <victor.le-corre@epita.fr>
2022-09-27 13:47:11 +0200

    [REFACTOR] Scheduler: Remove Dead Code

--- krakscheduler/scheduler.py
+++ krakscheduler/scheduler.py
@@ -18,7 +18,7 @@ class Scheduler:
             for i in range(len(stand.get_staff_needed())):
                 while(stand.get_free_places(i) > 0):
                     current_worker = random.choice(workers)
-                    while self.is_worker_currently_working(current_worker, i) != 0:
+                    while current_worker.is_working(i) != 0:
                         current_worker = random.choice(workers)
                     stand.add_worker(current_worker, i)
 
@@ -33,25 +33,3 @@ class Scheduler:
 
     def get_workers_list(self):
         return self.workers_list
-
-    # return the number of places left for the specific stand at a specific shift
-    def get_current_shift_remaining_places(self, shift_index, stand):
-        return stand.get_free_places(shift_index)
-
-    # return the number of places taken for the specific stand at a specific shift
-    def get_current_shift_taken_places(self, shift_index, stand):
-        return stand.get_taken_places(shift_index)
-
-    # return the list of current workers at a specific stand and at a specific shift
-    def get_current_shift_worker_list(self, shift_index, stand):
-        return stand.get_specific_shift_worker_list(shift_index)
-
-    #def create_stands_columns(self, nb_stands, stands_lists)
-
-    # check if the staff is already taken for a shift at a specific time or not
-    def is_worker_currently_working(self, worker, shift_index):
-        return worker.is_currently_staffing(shift_index)
-
-    # maximum hours of staff that a worker can do
-    def staff_already_worked_too_much(self, worker):
-        return worker.get_time_worked() >= 4
