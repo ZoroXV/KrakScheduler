75a403ba621755919cf584bb4a18f7164ff4c5a6
Victor LE CORRE <victor.le-corre@epita.fr>
2022-09-27 21:10:45 +0200

    [FEAT] xlsx_builder.py: Implement build_time_worked_sheet()

--- krakscheduler/xlsx_builder.py
+++ krakscheduler/xlsx_builder.py
@@ -10,6 +10,8 @@ def build_schedule(scheduler):
     time_worked_sheet = wb.add_worksheet('Worked Time')
 
     global_sheet.set_column(0,scheduler.get_event().get_hours_duration(), 30)
+    time_worked_sheet.set_column(0,scheduler.get_event().get_hours_duration(), 30)
+
     cell_format = wb.add_format({'bold': True, 'font_color': 'red', 'align': 'center'})
 
     build_stands_names(global_sheet, scheduler.get_event().get_stands_list(), cell_format)
@@ -56,4 +58,17 @@ def build_workers_shifts(sheet, stands_list):
         col += 1
 
 def build_time_worked_sheet(sheet, event):
-    pass
+    sheet.write(0, 0, 'Name')
+    sheet.write(0, 1, 'Hours Worked')
+
+    row = 1
+
+    data = []
+    for worker in event.get_workers_list():
+        data.append((worker.get_name(), worker.get_time_worked()))
+    data.sort(key=lambda tup: tup[1])
+
+    for name, time_worked in data:
+        sheet.write(row, 0, name)
+        sheet.write(row, 1, time_worked)
+        row += 1
