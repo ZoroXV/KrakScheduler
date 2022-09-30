090ebdaed921ef94b0cd5ee5e950b0f038465111
Victor LE CORRE <victor.le-corre@epita.fr>
2022-09-30 17:28:31 +0200

    [FEAT] main.py: Reduce Quantity of workers to challenge more the algorithm

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import datetime
 def main():
     event = ks.build_event()
 
-    for i in range(42):
+    for i in range(20):
         worker = ks.Worker("worker_" + str(i), [0,1,1,1,1,0,0])
         event.add_worker(worker)
 
-    for i in range(50, 100):
+    for i in range(20, 40):
         worker = ks.Worker("worker_" + str(i), [1,1,0,0,1,1,1])
         event.add_worker(worker)
 
@@ -21,7 +21,7 @@ def main():
 
     scheduler = ks.Scheduler(event)
     scheduler.fill_schedule()
-    scheduler.display_individual()
+    scheduler.display_global()
 
     wb, global_sheet, _ = ks.build_schedule(scheduler)
 
