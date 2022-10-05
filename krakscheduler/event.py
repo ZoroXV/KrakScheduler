4f7adb90b1bc71c3fa36a0f68d598091dd8c88ea
Victor LE CORRE <victor.le-corre@epita.fr>
2022-10-05 10:53:04 +0200

    [FIX] get_current_shift_free_workers(): Update condition on append
    Previous condition was only checking if the worker was present at
    shift_index and not if worker is present and available

--- krakscheduler/event.py
+++ krakscheduler/event.py
@@ -50,7 +50,7 @@ class Event:
     def get_current_shift_free_workers(self, shift_index):
         free_workers = []
         for worker in self.workers_list:
-            if worker.is_current_shift_present(shift_index):
+            if worker.is_current_shift_present(shift_index) and not worker.is_working(shift_index):
                 free_workers.append(worker)
         return free_workers
 
